9edc2f3bff787d537cbb84f8e689fe92d83e9baa
KAHO <84381486+michimichi2021@users.noreply.github.com>
2022-04-21 01:44:50 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// var val1 = "val変数";
+// console.log(val1);
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "val変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+// const val3 = "const変数を再宣言";
+
+// const val4 = {
+//   name: "かほ",
+//   age: 28,
+// };
+// val4.name = "kaho";
+// val4.addres = "Toyama";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
