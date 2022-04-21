4688828d8699589c7ef007a7e749871e838ef8c9
KAHO <84381486+michimichi2021@users.noreply.github.com>
2022-04-22 00:55:10 +0900

    デフォルト値 =　まで

--- src/index.js
+++ src/index.js
@@ -33,7 +33,77 @@
 // console.log(val4);
 
 // constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+//
+
+/**
+ *  テンプレート文字列
+ */
+
+// const name = "かほ";
+// const age = 23;
+
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+//
+
+/**
+ *
+ * アロー関数
+ *  */
+
+// function func1(str) {
+//   return str;
+// }
+
+// const func1 = function(str){
+//   return str;
+// }
+
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1,num2) => num1 + num2;
+
+// console.log(func3(10,20));
+
+// /*
+// *
+// *分割代入
+// */
+// const myProfile = {
+//   name: "かほ",
+//   age: 23,
+// };
+
+// const {name,age} = myProfile;
+// const message2 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message2);
+
+const myProfile = ["かほ", 23];
+const message3 =
+  "私の名前は" + myProfile[0] + "です。年齢は" + myProfile[1] + "です。";
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message4);
+
+/*
+ *
+ *デフォルト値、引数など
+ */
+
+const sayHello = (name = "かほ") => console.log("こんにちは!" + name + "さん!");
+sayHello();
+
+/**
+ *
+ * スプレッドシート*/
